2eb153870632d554f3bb5201714f6d1c9556a29e
ishansubedi <ishan@appmozart.com>
2020-03-06 22:40:44 -0800

    updated packages

--- firefly-select.js
+++ firefly-select.js
@@ -1,32 +1,30 @@
-import { PolymerElement} from '@polymer/polymer/polymer-element.js';
-import { html } from '@polymer/polymer/lib/utils/html-tag.js';
-import { afterNextRender } from '@polymer/polymer/lib/utils/render-status.js';
+import { PolymerElement } from "@polymer/polymer/polymer-element.js";
+import { html } from "@polymer/polymer/lib/utils/html-tag.js";
+import { afterNextRender } from "@polymer/polymer/lib/utils/render-status.js";
 
-import '@polymer/paper-dropdown-menu/paper-dropdown-menu.js';
-import '@polymer/paper-listbox/paper-listbox.js';
-import '@polymer/paper-item/paper-item.js';
-import '@vaadin/vaadin-dropdown-menu/src/vaadin-dropdown-menu.js';
-import '@vaadin/vaadin-item/src/vaadin-item.js';
-import '@vaadin/vaadin-list-box/src/vaadin-list-box.js';
-import '@polymer/neon-animation/neon-animation.js';
-import '@polymer/iron-flex-layout/iron-flex-layout.js';
+import "@polymer/paper-dropdown-menu/paper-dropdown-menu.js";
+import "@polymer/paper-listbox/paper-listbox.js";
+import "@polymer/paper-item/paper-item.js";
+import "@vaadin/vaadin-dropdown-menu/src/vaadin-dropdown-menu.js";
+import "@vaadin/vaadin-item/src/vaadin-item.js";
+import "@vaadin/vaadin-list-box/src/vaadin-list-box.js";
+import "@polymer/neon-animation/neon-animation.js";
+import "@polymer/iron-flex-layout/iron-flex-layout.js";
 
-import '@firefly-elements/polymerfire/firebase-query.js';
-import '@firefly-elements/polymerfire/firebase-auth.js';
-import '@firefly-elements/polymerfire/firebase-database-behavior.js';
+import "@firefly-elements/polymerfire/firestore-query.js";
+import "@firefly-elements/polymerfire/firebase-auth.js";
+import "@firefly-elements/polymerfire/firebase-database-behavior.js";
 
-// import './aspen-list-icons.js'
-import '@aspen-elements/aspen-list-icons';
+import "@aspen-elements/aspen-list-icons";
 
-import '@aspen-elements/aspen-button';
-import {FireflyListMixin} from '@firefly-elements/firefly-list-mixin';
-
+import "@aspen-elements/aspen-button";
+import { FireflyListMixin } from "@firefly-elements/firefly-list-mixin";
 
 /**
  * `firefly-select` This component is designed to display a list of values where the list itself is editable,
  * and has a firebase backing list. For example, this can be used to display a list of therapeutic areas, and
  * let the user add new therapeutic areas without having to switch pages.
- * 
+ *
  * <firefly-select label="Whatevers" app-name="whatever" path="/whatever" order-by-child="something" selected="{{selected}}">
  *
  * @summary ShortDescription.
@@ -34,53 +32,65 @@ import {FireflyListMixin} from '@firefly-elements/firefly-list-mixin';
  * @polymer
  * @extends {Polymer.Element}
  */
-class AspFireList extends FireflyListMixin(PolymerElement) {
+class FireflySelect extends FireflyListMixin(PolymerElement) {
   static get template() {
     return html`
-        <style>
-            :host {
-                display: block
-            }
-            .component{
-                @apply --layout-horizontal;
-            }
-            aspen-button{
-               margin-top: 35px;
-               margin-left: 4px;
-                height: 24px;
-            }
-        </style>
+      <style>
+        :host {
+          display: block;
+        }
+        .component {
+          @apply --layout-horizontal;
+        }
+        aspen-button {
+          margin-top: 35px;
+          margin-left: 4px;
+          height: 24px;
+        }
+      </style>
 
-        <firebase-query app-name="[[appName]]" path="[[path]]" data="{{model}}" order-by-child="[[orderByChild]]" equal-to="[[equalTo]]"></firebase-query>
+      <fs-query
+        app-name="[[appName]]"
+        path="[[path]]"
+        data="{{model}}"
+      ></fs-query>
 
-        <div class="component">
-            <vaadin-dropdown-menu label="[[label]]" placeholder="[[placeholder]]" value="{{selected}}" on-value-changed="__valueChanged">
-                <template>
-                    <vaadin-list-box>
-                        <template is="dom-repeat" items="[[model]]">
-                            <vaadin-item id="[[item.\$key]]" value="[[item]]">[[item.name]]</vaadin-item>
-                        </template>
-                    </vaadin-list-box>
-                </template>
-            </vaadin-dropdown-menu>
-        
-            <template is="dom-if" if="{{editable}}">
-                <aspen-button icon="list:add-circle" on-tap="_openAddDialog"></aspen-button>
-            </template>
-           
-        </div>
+      <div class="component">
+        <vaadin-dropdown-menu
+          label="[[label]]"
+          placeholder="[[placeholder]]"
+          value="{{selected}}"
+          on-value-changed="__valueChanged"
+        >
+          <template>
+            <vaadin-list-box>
+              <template is="dom-repeat" items="[[model]]">
+                <vaadin-item id="[[item.$key]]" value="[[item]]"
+                  >[[item.name]]</vaadin-item
+                >
+              </template>
+            </vaadin-list-box>
+          </template>
+        </vaadin-dropdown-menu>
 
-        <slot select=".detail-dialog"></slot>
-`;
+        <template is="dom-if" if="{{editable}}">
+          <aspen-button
+            icon="list:add-circle"
+            on-tap="_openAddDialog"
+          ></aspen-button>
+        </template>
+      </div>
+
+      <slot select=".detail-dialog"></slot>
+    `;
   }
 
   /**
    * String providing the tag name to register the element under.
    */
   static get is() {
-      return 'firefly-select';
+    return "firefly-select";
   }
-
 
   /**
    * Instance of the element is created/upgraded. Use: initializing state,
@@ -88,18 +98,16 @@ class AspFireList extends FireflyListMixin(PolymerElement) {
    * @constructor
    */
   constructor() {
-      super();
+    super();
   }
 
   /**
-   * Use for one-time configuration of your component after local DOM is initialized. 
+   * Use for one-time configuration of your component after local DOM is initialized.
    */
   ready() {
-      super.ready();
+    super.ready();
 
-      afterNextRender(this, function() {
-          
-      });
+    afterNextRender(this, function() {});
   }
 
   /**
@@ -108,26 +116,26 @@ class AspFireList extends FireflyListMixin(PolymerElement) {
    * that object to any listener.
    * @param {Event} e the event object
    */
-  __valueChanged(e){
+  __valueChanged(e) {
+    let modelMap = new Map();
+    for (let item of this.model) {
+      modelMap.set(item.$key, item);
+    }
 
-      let modelMap = new Map();
-      for(let item of this.model){
-          modelMap.set(item.$key, item);
-      }
+    let selectedValue = e.detail.value;
+    let selectedItem = modelMap.get(selectedValue.$key);
+    this.set("selectedItem", selectedItem);
 
-      let selectedValue = e.detail.value;
-      let selectedItem = modelMap.get(selectedValue.$key);
-      this.set('selectedItem', selectedItem);
-
-      this.dispatchEvent(new CustomEvent('fire-list-item-changed', {
-          bubbles: true,
-          composed: true,
-          detail: {
-              value: selectedItem
-              
-          }
-      }));
+    this.dispatchEvent(
+      new CustomEvent("fire-list-item-changed", {
+        bubbles: true,
+        composed: true,
+        detail: {
+          value: selectedItem
+        }
+      })
+    );
   }
 }
 
-window.customElements.define(AspFireList.is, AspFireList);
+window.customElements.define(FireflySelect.is, FireflySelect);
